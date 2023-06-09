65aebd2434cb122a0911504b8c44aeb7e01c3b99
VitorSiilva2 <vitoralves450@gmail.com>
2023-06-09 08:14:46 -0300

    Excetion, Properties e DB

---
+++ src/main/java/org/example/db/DB.java
@@ -0,0 +1,40 @@
+package org.example.db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DB {
+
+    private static Connection conn = null;
+
+    // Open to connection
+    public static Connection getConnection() {
+        if (conn == null) {
+            try {
+                Properties props = LoadProperties();
+                String url = props.getProperty("dburl");
+                conn = DriverManager.getConnection(url, props);
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+        return conn;
+
+    }
+
+    private static Properties LoadProperties() {
+        try (FileInputStream fs = new FileInputStream("db.properties")) {
+            Properties props = new Properties();
+            props.load(fs);
+            return props;
+        } catch (IOException e) {
+            throw new DbException(e.getMessage());
+        }
+    }
+
+
+}
